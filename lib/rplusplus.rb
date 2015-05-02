4c6c8b6c5f9ffeeed8cb808df2f6525d46b8f37d
Daniel Smith <jellymann@gmail.com>
2015-05-02 18:03:13 +0200

    feat: add rplusplus call the calls the specified command from argv

--- lib/rplusplus.rb
+++ lib/rplusplus.rb
@@ -7,4 +7,8 @@ require "rplusplus/commands/generate"
 require "rplusplus/generators/class_generator"
 
 module RPlusPlus
+  def self.call argv
+    command, *arguments = *argv
+    Command.call command.to_sym, *arguments
+  end
 end
